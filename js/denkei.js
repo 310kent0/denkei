b51a9f1b677513b5968527ae86244b2998626619
310kent0 <bokuwazettainiryunennsimashen@gmail.com>
2017-05-09 23:07:35 +0900

    denkei.jsの非同期処理ã化

--- js/denkei.js
+++ js/denkei.js
@@ -1,52 +1,120 @@
 // でんけいの開発者はでんけい及びそのソースコードのどんな使い方に対しても文句をつけたり、法的措置を取ったりしません。自由に使って下さい。ただし、でんけいの開発者はいかなる責任も負いません。
 var log_dir = './log/';
 
-var next = 1;
+var next_load = 1;
+var next_write = 1;
+var log_list = {};
 
 function $denkei_reader(log_json) {
 	localforage.setItem(log_json.index, log_json.message);
 };
 
-function denkei_loader(index, length) {
-	if (length === undefined) {
-		$.ajaxSetup({
-			cache : false
+function get_length() {
+	var dfd = $.Deferred();
+
+	$.ajaxSetup({
+		cache : false
+	});
+
+	$.get(log_dir + 'length.txt').done(function(length) {
+		localforage.setItem('length', length).then(function() {
+			dfd.resolve(parseInt(length));
+		}).catch(function(err) {
+			dfd.reject(err);
 		});
-		$.get(log_dir + 'length.txt').done(function(length) {
-			$.ajaxSetup({
-				cache : true
-			});
-			localforage.setItem('length', length);
-			denkei_loader(index, parseInt(length));
-		}).fail(function() {
-			localforage.getItem('length').then(function(length) {
-				if (length !== null) {
-					denkei_loader(index, parseInt(length));
-				}
-			});
-		});
-	} else if (index <= length) {
-		localforage.getItem(String(index)).then(function(message) {
-			if (message === null) {
-				$.getScript(log_dir + index + '.js').done(function() {
-					denkei_loader(index, length);
-				});
+	}).fail(function() {
+		localforage.getItem('length').then(function(length) {
+			if (length == null) {
+				dfd.reject('length is null.');
 			} else {
-				var log_area = $('<pre></pre>').text(index + ':' + message);
-				$('#log_box').prepend(log_area);
-				log_area.ready(function() {
-					denkei_loader(++next, length);
-				});
+				dfd.resolve(parseInt(length));
 			}
+		}).catch(function(err) {
+			dfd.reject(err);
 		});
-	} else {
-		$('button').prop('disabled', false);
+		;
+	});
+
+	$.ajaxSetup({
+		cache : true
+	});
+
+	return dfd.promise();
+}
+
+function denkei_loader_depth3(index) {
+	var dfd = $.Deferred();
+
+	localforage.getItem(String(index)).then(function(message) {
+		if (message == null) {
+			$.getScript(log_dir + index + '.js').then(function() {
+				localforage.getItem(String(index)).then(function(message) {
+					if (message == null) {
+						dfd.reject('message is null.');
+					} else {
+						log_list[String(index)] = $('<pre></pre>').text(index + ':' + message);
+						dfd.resolve();
+					}
+				}).catch(function(err) {
+					dfd.reject(err);
+				});
+			}).fail(function(err) {
+				dfd.reject(err);
+			});
+		} else {
+			log_list[String(index)] = $('<pre></pre>').text(index + ':' + message);
+			dfd.resolve();
+		}
+	}).catch(function(err) {
+		dfd.reject(err);
+	});
+
+	return dfd.promise();
+}
+
+function denkei_loader_depth2(length) {
+	var dfd = $.Deferred();
+	var load_array = [];
+
+	for (var i = 0; next_load + i <= length; i++) {
+		load_array[i] = denkei_loader_depth3(next_load + i);
 	}
-};
+
+	$.when.apply($, load_array).done(function() {
+		dfd.resolve(length);
+	}).fail(function(err) {
+		dfd.reject(err);
+	});
+
+	return dfd.promise();
+}
+
+function denkei_writer(length) {
+	for (var i = 0; next_write + i <= length; i++) {
+		$('#log_box').prepend(log_list[String(next_write + i)]);
+	}
+}
+
+function denkei_loader_depth1() {
+	get_length().then(denkei_loader_depth2).done(function(length) {
+		denkei_writer(length);
+
+		next_load = length + 1;
+		next_write = length + 1;
+
+		$('#log_box').ready(function() {
+			$('button').prop('disabled', false);
+		});
+	}).fail(function(err) {
+		alert('エラー発生:' + err);
+		console.log(err);
+		$('button').prop('disabled', false);
+	});
+}
 
 $(function() {
 	$('button').prop('disabled', true);
-	denkei_loader(next);
+	denkei_loader_depth1();
 
 	$('#write').click(function() {
 		$('button').prop('disabled', true);
@@ -57,9 +125,11 @@ $(function() {
 			}
 		}).done(function() {
 			setTimeout(function() {
-				denkei_loader(next);
+				denkei_loader_depth1();
 			}, 1);
-		}).always(function() {
+		}).fail(function(err) {
+			alert('エラー発生:' + err);
+			console.log(err);
 			$('button').prop('disabled', false);
 		});
 		$('#message').val('');
@@ -67,14 +137,19 @@ $(function() {
 
 	$('#update').click(function() {
 		$('button').prop('disabled', true);
-		denkei_loader(next);
+		denkei_loader_depth1();
 	});
 
 	$('#clear').click(function() {
 		$('button').prop('disabled', true);
 		$('#log_box').text('');
 		localforage.clear().then(function() {
-			next = 1;
+			next_load = 1;
+			next_write = 1;
+			$('button').prop('disabled', false);
+		}).catch(function(err) {
+			alert('エラー発生:' + err);
+			console.log(err);
 			$('button').prop('disabled', false);
 		});
 	});
